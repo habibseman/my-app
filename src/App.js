b82d6bc9fb6e911ab5128275a47c1d12d18fdc0d
Habib Seman <habibseman032@gmail.com>
2023-01-14 22:55:48 +0300

    Add button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
+const name = "";
 function App() {
+    
+    const onClick = () => {
+	alert("hey this is me")
+    }
   return (
     <div className="App">
-      <h1>hello react!</h1>
+	  <h1>hello react!</h1>
+	  <button onClick={onClick}>SEND DATA</button>
+	  <h2>my name is {name}</h2>
     </div>
   );
 }
